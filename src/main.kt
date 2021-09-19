90dae73a7947801b18eb992d130b869953772081
jmcmahon38 <jmcmahon38@students.mchenry.edu>
2021-09-19 13:12:08 -0500

    added exit menu and option for user to continue asking for translation

--- src/main.kt
+++ src/main.kt
@@ -1,21 +1,38 @@
 fun main(args: Array<String>) {
-    var englishNum = 3
+    var englishNum = 0
     var spanishNum = "x"
+    var exit = false
+    while (exit != true) {
+        print("Enter a number 1-10 for the spanish spelling of the number\n")
+        englishNum = readLine()!!.toInt()
+        when (englishNum) {
+            1 -> spanishNum = "uno"
+            2 -> spanishNum = "dos"
+            3 -> spanishNum = "tres"
+            4 -> spanishNum = "quatro"
+            5 -> spanishNum = "cinco"
+            6 -> spanishNum = "seis"
+            7 -> spanishNum = "siete"
+            8 -> spanishNum = "ocho"
+            9 -> spanishNum = "nueve"
+            10 -> spanishNum = "diez"
+            !in 1..10 -> spanishNum = "Out of Range"
+            else -> spanishNum = "Unknown"
+        }
 
-    when (englishNum) {
-        1 -> spanishNum = "uno"
-        2 -> spanishNum = "dos"
-        3 -> spanishNum = "tres"
-        4 -> spanishNum = "quatro"
-        5 -> spanishNum = "cinco"
-        6 -> spanishNum = "seis"
-        7 -> spanishNum = "siete"
-        8 -> spanishNum = "ocho"
-        9 -> spanishNum = "nueve"
-        10 -> spanishNum = "diez"
-        !in 1..10 -> spanishNum = "Out of Range"
-        else -> spanishNum = "Unknown"
+        println("$englishNum es $spanishNum en español ")
+        print("would you like to check another number y/n? \n")
+        var answer: String? = readLine()
+        if (answer?.lowercase() == "n"){
+            exit = true
+            println("Exiting now...")}
+        else if (answer?.lowercase() == "y") {
+            println("next number...")
+        }
+        else{
+            println("That is not a valid answer")
+            println("next number...")
+        }
+
     }
-
-    println("$englishNum es $spanishNum en español ")
 }
